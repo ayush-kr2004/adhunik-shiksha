8086ad735516b7e37a466f7d2a3894398e3a3314
Ayush <amankumarerka@gmail.com>
2026-02-17 23:29:35 +0530

    working on frontend starting and manage to implement tailwind conffiguration

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 import "./env.js";
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import userRouter from "./routes/user.js";
 import adminRouter from "./routes/admin.js";
 import courseRouter from "./routes/course.js";
 
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/user", userRouter);
